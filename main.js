1e389b0b559d38d811e2ed688bb1c8f975e83532
utri092 <utsavtrivedi16@gmail.com>
2019-12-08 19:37:18 +1300

    Changed electron

--- main.js
+++ main.js
@@ -47,10 +47,10 @@ const http = require('http');
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<H1>Hello World!</H1>');
+  response.end('<H1>Hello Monkey!</H1>');
 }).listen(process.env.PORT);
 
-console.log('App is running…');
+console.log('App is running…Electron app!');
 //xxxxxxxxxxxxxxxxxxxxxxxxxxx////////
 
 app.on('ready', createWindow)
