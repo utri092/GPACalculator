b1397d3c8a3a732be5862b892fc8e49b95a7b2bf
utri092 <utsavtrivedi16@gmail.com>
2019-12-08 19:45:42 +1300

    Changed hello world text

--- main.js
+++ main.js
@@ -37,20 +37,20 @@ function createWindow () {
 
 
 // Disable chromium extensions
-const browser = await puppeteer.launch({
-  ignoreDefaultArgs: ['--disable-extensions'],
-});
+// const browser = await puppeteer.launch({
+//   ignoreDefaultArgs: ['--disable-extensions'],
+// });
 
 
 // Server listen
-const http = require('http');
+//const http = require('http');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<H1>Hello Monkey!</H1>');
-}).listen(process.env.PORT);
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/html'});
+//   response.end('<H1>Hello Monkey!</H1>');
+// }).listen(process.env.PORT);
 
-console.log('App is running…Electron app!');
+//console.log('App is running…Electron app!');
 //xxxxxxxxxxxxxxxxxxxxxxxxxxx////////
 
 app.on('ready', createWindow)
