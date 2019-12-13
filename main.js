0f874824a5b087e36abfaab5194149acbc4b0f8a
utri092 <utsavtrivedi16@gmail.com>
2019-12-14 10:56:07 +1300

    HTML attempt 1

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow () {
 
   // and load the render.html of the app.
   try {
-    mainWindow.loadFile('render.html')
+    mainWindow.loadFile('./render.html')
   } catch (error) {
     console.log(error)
   }
@@ -48,6 +48,8 @@ const http = require('http');
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('<H1>Hello Monkey!</H1>');
+  //response.write(html);
+  //response.end()
 }).listen(process.env.PORT);
 
 console.log('App is running…Electron app!');
